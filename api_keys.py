4b06bca7ad5c36c1b43e0e3936fa489b9a881a6a
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2016-03-11 18:49:54 +0530

    add api_keys file

---
+++ api_keys.py
@@ -0,0 +1,11 @@
+import random
+
+#https://free-dashboard.rome2rio.com/Account/Register
+rome_2_rio = ['IWnbSA2f', '670LizCP','PvXZgbor','MDWzbA6C']
+def rio_key():
+    return random.choice(rome_2_rio)
+
+google = ['AIzaSyBYNhvCYTT7iIZNKavmTf9lplS57WQeCJw','AIzaSyBRYZvFpZjK4GPwQFcWV4A2OB_60sEvg6E']
+def goo_key():
+    return random.choice(google)
+
